a56d6e9c6172d78285fcd0f2f1f39ca229141a4e
kchia <kchia87@gmail.com>
2021-09-20 12:12:33 -0400

    update variables examples

---
+++ 01-variables.js
@@ -0,0 +1,65 @@
+// VARIABLE UPDATE
+// var color = "red";
+// var color = "yellow";
+// console.log(color);
+
+// let
+// let color = "red";
+// let color = "yellow";
+// console.log(color);
+
+// let color = "red";
+// color = "yellow";
+// console.log(color);
+
+// const
+// const color = "red";
+// color = "yellow";
+// console.log(color);
+
+// const computer = {
+//   type: "Macintosh",
+//   os: "Catalina",
+// };
+
+// computer.type = "Microsoft";
+// computer.os = "Windows 10";
+// computer = {
+//   type: "Microsoft",
+// };
+
+// console.log(computer);
+
+// VARIABLE SCOPE
+// let fruit = "apple";
+// let fruitEaten = 3;
+
+if (fruitEaten > 2) {
+  let fruit = "orange";
+  console.log(fruit);
+}
+
+// for(let i = 0; i < 9; i++) {
+
+// }
+
+// let shouldEatFruit = true;
+// if (shouldEatFruit) {
+//   let fruit = "orange";
+//   const anotherFruit = "apple";
+//   console.log("fruit inside block: ", fruit);
+//   console.log("anotherFruit inside block: ", anotherFruit);
+// }
+
+// console.log("fruit outside block: ", fruit);
+// console.log("anotherFruit outside block:", anotherFruit);
+
+// VARIABLE HOISTING
+// console.log(animal);
+// var animal = "rabbit";
+
+// console.log(animal);
+// let animal = "rabbit";
+
+console.log(flower);
+const flower = "rose";
