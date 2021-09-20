6ad6568e701e25b5466ccafb2a7987a58731791e
kchia <kchia87@gmail.com>
2021-09-20 12:53:20 -0400

    add strings examples

---
+++ 02-strings.js
@@ -0,0 +1,44 @@
+// NEW STRING METHODS
+const repeated = "hello world ".repeat(5);
+console.log(repeated);
+
+// console.log("hello".indexOf("sdfdhel"));
+console.log("hello".includes("asdfel"));
+
+// TEMPLATE LITERALS
+
+// ES5
+// var greeting = "hello";
+// var name = "Hou";
+
+// var sayHello = greeting + " " + name;
+// console.log(sayHello);
+
+// ES6
+// const greeting = "hello";
+// const name = "Hou";
+
+// const sayHello = `The greeting say: ${greeting} ${name}`;
+// console.log(sayHello);
+
+const multiLineString = `
+Hello,
+my friend,
+how are you, doing?
+`;
+
+console.log(multiLineString);
+
+// Challenge: Rewrite the following to ES6
+const greeting = "Good morning";
+const weather = "sunny";
+
+// ES5
+const sentence =
+  greeting + ", how are you doing?" + " The weather is " + weather + " today.";
+
+// ES6
+
+const sentenceES6 = `${greeting}, how are you doing? The weather is ${weather} today.`;
+
+console.log(sentenceES6);
