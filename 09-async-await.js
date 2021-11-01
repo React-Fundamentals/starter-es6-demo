728fbde3ff49a2c611ac29441273e8cdfe335c30
kchia <kchia87@gmail.com>
2021-11-01 17:53:02 -0400

    add async await code

--- 09-async-await.js
+++ 09-async-await.js
@@ -21,11 +21,15 @@ async function fetchTriviaWithAsyncAwait() {
     const response = await fetch(
       "https://opentdb.com/api.php?amount=1&category=18"
     );
+    console.log("in between fetch and response.json()");
     const data = await response.json();
     console.log(data);
+    console.log("hello world 3");
   } catch (error) {
     console.log(error);
   }
 }
 
-fetchTriviaWithAsyncAwait();
+fetchTriviaWithAsyncAwait(); // data
+console.log("hello world");
+console.log("hello world 2");
